2a5d111f96a8818c8e0580cd2b12a4540a50f2ba
beyond_dream <2265508387@qq.com>
2016-06-07 16:58:05 +0800

    加了注释

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*	git test 2016/6/7
+*/ 
+$i = 1464657731;
+$i_2 = 1464434079;
+$i_3 = 1464664300;
+$i_4 = 1464664213;
+
+
+echo date('Y-m-d H:i:s', $i);
+echo date('Y-m-d H:i:s', $i_2);
+echo date('Y-m-d H:i:s', $i_3);
+echo date('Y-m-d H:i:s', $i_4);
+
+
+phpinfo();
